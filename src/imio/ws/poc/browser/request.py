d452b050c5a2d46b016d9165f064be405be0e6f5
Martin Peeters <martin.peeters@affinitic.be>
2015-01-28 16:05:04 +0100

    Use the base views from imio.wsrequest.core

--- src/imio/ws/poc/browser/request.py
+++ src/imio/ws/poc/browser/request.py
@@ -1,24 +1,22 @@
 # encoding: utf-8
 
-from Products.Five.browser import BrowserView
-from imio.wsrequest.core import Request
-from imio.wsrequest.core import Response
-from imio.wsrequest.core import RequestException
-import json
+from imio.wsrequest.core import WSRequestBaseView
+from imio.wsrequest.core import WSResponseBaseView
 
 
-class WSRequestView(BrowserView):
+class WSRequestView(WSRequestBaseView):
 
-    def render(self):
-        self.request.response.setHeader('Content-Type', 'application/json')
-        return json.dumps({
-            'success': self.success,
-            'error': self.error,
-            'id': self.id,
-        })
+    # XXX To be moved in a configuration view
+    plone_id = 'plone2'
+    application_id = 'Plone'
+    request_type = 'create'
+
+    render_extra_values = {
+        'id': None,
+    }
 
     @property
-    def content_dict(self):
+    def request_kwargs(self):
         return {
             'type': self.context.portal_type,
             'title': self.context.Title(),
@@ -30,44 +28,9 @@ class WSRequestView(BrowserView):
             },
         }
 
-    def __call__(self):
-        request = Request('plone2', 'Plone', 'create', **self.content_dict)
-        request.webservice = 'test_request'
-        request.version = 0.1
 
-        self.success = False
-        self.id = None
-        self.error = None
-        try:
-            self.success, self.id = request.do_request()
-        except RequestException, e:
-            self.error = e.message
-        return self.render()
+class WSResponseView(WSResponseBaseView):
 
-
-class WSResponseView(BrowserView):
-
-    def render(self):
-        self.request.response.setHeader('Content-Type', 'application/json')
-        return json.dumps({
-            'success': self.success,
-            'error': self.error,
-            'url': self.url,
-        })
-
-    def __call__(self):
-        request = Response(self.request.get('id'))
-        request.webservice = 'test_response'
-        request.version = 0.1
-
-        self.success = False
-        self.error = None
-        self.url = None
-        try:
-            self.success, response = request.do_request()
-            self.url = response.get('url', None)
-            if response.get('external_uid'):
-                self.context.external_uid = response.get('external_uid')
-        except RequestException, e:
-            self.error = e.message
-        return self.render()
+    render_extra_values = {
+        'url': None,
+    }
